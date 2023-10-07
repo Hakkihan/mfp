d6601f5bf1182ad2888db9786045043de5034a48
Hakki Tunbak <tunbak.hakkihan@gmail.com>
2023-10-07 14:26:10 +0100

    comitheader

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,16 +2,21 @@ import React from "react";
 // import { ReactDOM } from "react";
 // import {mount} from 'marketing/MarketingApp';
 import MarketingApp from "./components/MarketingApp";
+import { BrowserRouter } from 'react-router-dom';
+import Header from "./components/Header";
 
 // console.log(mount);
 
 export default () => {
     return (
-        <div>
-            <h1>Hi there!!!</h1>      
-        <hr />
-        <MarketingApp />
-        </div>
-        
+        <BrowserRouter>
+            <div>
+                <Header />
+                <MarketingApp />
+            </div>
+        </BrowserRouter>
+
+
+
     );
 }
